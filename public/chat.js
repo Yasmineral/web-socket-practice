84bec50103d546a95334f48da259eb1a11fa2732
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-04-17 12:24:08 +0100

    add socket emit message for keypress

--- public/chat.js
+++ public/chat.js
@@ -2,10 +2,11 @@ var socket = io.connect("http://localhost:4000");
 
 // Query DOM
  
-let message = document.getElementById('message');
-let handle = document.getElementById('handle');
-let btn = document.getElementById('send');
-let output = document.getElementById('output');
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 // Emit events
 
@@ -21,3 +22,11 @@ btn.addEventListener('click', () => {
 socket.on('chat', (data) => {
   output.innerHTML += "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>"
 })
+
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+})
+
+socket.on('typing', (data) => {
+  feedback.innerHTML = "<p><em>" + data + " is typing a message..." + "</em></p>"
+}) 
